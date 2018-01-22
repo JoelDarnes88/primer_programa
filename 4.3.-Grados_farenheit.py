d0ced73b9d81b004f204aae4c9724e0558ee21a2
JoelDarnes88 <joeldarnes@gmail.com>
2018-01-23 00:28:48 +0100

    Añadiendo primeros proyectos

---
+++ 4.3.-Grados_farenheit.py
@@ -0,0 +1,5 @@
+print("Bienvenido sl convertor de grados celsius a grados farenheit")
+grados_farenheit = int(input("¿Cuantos grados farenheit quieres pasar a centigrados?"))
+grados_celsius = ((grados_farenheit - 32)/1.8)
+print("{} grados farenheit".format(grados_farenheit), "son igual a {} grados celsius".format(grados_celsius)
+      )
